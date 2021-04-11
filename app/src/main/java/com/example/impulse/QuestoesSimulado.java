c8d184c87fd7fa0556a3632b7b9b648c557a1cba
Juliana Alba <jualba.ti@gmail.com>
2021-04-11 13:55:49 -0300

    Criação de layout (questões do quizz e ranking) e adição de funcionalidade no botão da classe Simulado

--- app/src/main/java/com/example/impulse/QuestoesSimulado.java
+++ app/src/main/java/com/example/impulse/QuestoesSimulado.java
@@ -2,13 +2,32 @@ package com.example.impulse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuestoesSimulado extends AppCompatActivity {
+    Button bEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questoes_simulado);
+
+        bEnviar= (Button) findViewById(R.id.bEnviar);
+
+        bEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Simulado.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
     }
 }
