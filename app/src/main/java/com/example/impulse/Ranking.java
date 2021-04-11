92603d8bed2ba34b0f897521bd310f7284019587
Juliana Alba <jualba.ti@gmail.com>
2021-04-10 21:17:48 -0300

    Criação das classes Ranking e Questões do Simulado. Adicionado funcionalidade nos botões da classe Simulado

--- app/src/main/java/com/example/impulse/Ranking.java
+++ app/src/main/java/com/example/impulse/Ranking.java
@@ -3,6 +3,8 @@ package com.example.impulse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Ranking extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Ranking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/impulse/Simulado.java
+++ app/src/main/java/com/example/impulse/Simulado.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Simulado extends AppCompatActivity {
@@ -12,6 +13,10 @@ public class Simulado extends AppCompatActivity {
     ImageButton btsf;
     ImageButton btsv;
     ImageButton btsa;
+    ImageButton btiranking;
+    Button btranking;
+    ImageButton btsimulado1;
+    ImageButton btsimulado2;
 
 
     @Override
@@ -24,6 +29,10 @@ public class Simulado extends AppCompatActivity {
         btsf = (ImageButton) findViewById(R.id.btsf);
         btsv = (ImageButton) findViewById(R.id.btsv);
         btsa = (ImageButton) findViewById(R.id.btsa);
+        btiranking = (ImageButton) findViewById(R.id.btiranking);
+        btranking = (Button) findViewById(R.id.btranking);
+        btsimulado1 = (ImageButton) findViewById(R.id.btsimulado1);
+        btsimulado2 = (ImageButton) findViewById(R.id.btsimulado2);
 
 
         btsp.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +77,54 @@ public class Simulado extends AppCompatActivity {
             public void onClick(View view) {
 
                 Intent intent = new Intent(getApplicationContext(), Audio.class); //IR PARA O AUDIO
+                startActivity(intent);
+
+
+
+            }
+        });
+
+        btiranking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Ranking.class); //IR PARA O RANKING
+                startActivity(intent);
+
+
+
+            }
+        });
+
+        btranking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Ranking.class); //IR PARA O RANKING
+                startActivity(intent);
+
+
+
+            }
+        });
+
+        btsimulado1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), QuestoesSimulado.class); //IR PARA AS QUESTOES DO SIMULADO
+                startActivity(intent);
+
+
+
+            }
+        });
+
+        btsimulado2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), QuestoesSimulado.class); //IR PARA AS QUESTOES DO SIMULADO
                 startActivity(intent);
 
 
